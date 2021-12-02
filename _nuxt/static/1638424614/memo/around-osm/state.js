76dcf83375bd275b0287181df48b8ab4fe659b2d
otkzh <otkzh@users.noreply.github.com>
2021-12-02 05:57:01 +0000

    deploy: 4bbd673ca39495768737189a5d14dc942dc46bcb

--- _nuxt/static/1638424614/memo/around-osm/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638424614",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmemo\u002Faround-osm",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cd38f788"}}}}(null));

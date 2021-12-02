4873069e71713ee58042e55e3bf021cf88048b1c
otkzh <otkzh@users.noreply.github.com>
2021-12-02 05:56:55 +0000

    deploy: 8f487970d9641c08c1a1cd9c66530313d5a58fa9

---
+++ _nuxt/static/1638424614/akibito/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638424614",layout:"default",error:a,serverRendered:true,routePath:"\u002Fakibito",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cd38f788"}}}}(null));

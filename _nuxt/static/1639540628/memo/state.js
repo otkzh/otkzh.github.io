37f638d719aa7f7fa1fde623ad1d469808b31926
otkzh <otkzh@users.noreply.github.com>
2021-12-15 03:57:38 +0000

    deploy: c47bf55fd220ebff58773748bca32d344f26637b

--- _nuxt/static/1639540628/memo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639540628",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmemo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fac7e383"}}}}(null));

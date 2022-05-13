f43beba58c17e153fb3c3206faf442aaa5381aff
otkzh <otkzh@users.noreply.github.com>
2022-05-13 15:36:07 +0000

    deploy: ccb44bf96f6ec8774b3f94568c30d2f862aa5c33

---
+++ _nuxt/static/1652456166/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652456166",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b1a740a9"}}}}(null,"\u002F"));

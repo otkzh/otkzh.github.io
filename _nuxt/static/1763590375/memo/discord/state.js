804600eb73c9a8cdb01db428d367cd8eb97d4407
otkzh <otkzh@users.noreply.github.com>
2025-12-14 09:40:40 +0000

    deploy: b90b1cc4aec534937c95cdcc88f395a7283cbcd2

--- _nuxt/static/1763590375/memo/discord/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1763590375",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmemo\u002Fdiscord",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ddf15218"}}}}(null));
